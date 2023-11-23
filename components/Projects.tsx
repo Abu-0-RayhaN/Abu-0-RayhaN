41800fd09fefdd8975b0995a8e612493a31438ea
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-11-23 22:37:42 +0600

    project add

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,6 +6,7 @@ import noorShop from "../public/img/projects/nike-store.png";
 import dynamicPortfolio from "../public/img/projects/tea-house.png";
 import reactBD from "../public/img/projects/service.png";
 import dashboard from "../public/img/projects/usability-hub.png";
+import newsportal from "../public/img/projects/news-portal.png";
 import Image from "next/image";
 import ProjectCard from "./ProjectCard";
 
@@ -14,6 +15,31 @@ const Projects = () => {
     <div className="wrapper">
       <Title text="Projects" icon={<FaProjectDiagram />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
+        <ProjectCard
+          img={newsportal.src}
+          title="News24 Portal"
+          link="https://news-24-portal.netlify.app/"
+        />
+            <ProjectCard
+              img={noorShop.src}
+              title="Nike Clone"
+              link="https://shop-nike1.netlify.app/"
+            />
+          <ProjectCard
+            img={cyberBlog.src}
+            title="HooBank"
+            link="https://curious-pika-ec3e13.netlify.app/"
+          />
+           <ProjectCard
+          img={dynamicPortfolio.src}
+          title="Tea House"
+          link="https://abu-0-rayhan.github.io/Tea-House/"
+        />
+        <ProjectCard
+          img={dashboard.src}
+          title="Usability Hub"
+          link="https://helpful-bubblegum-1cb14f.netlify.app/"
+        />
         <ProjectCard
           img={amazonClone.src}
           title="Amazon Clone"
@@ -24,27 +50,9 @@ const Projects = () => {
           title="Services Provider"
           link="https://abu-0-rayhan.github.io/Tailwind-Starter-Project-with-darkMode/"
         />
-        <ProjectCard
-          img={cyberBlog.src}
-          title="HooBank"
-          link="https://curious-pika-ec3e13.netlify.app/"
-        />
-        <ProjectCard
-          img={noorShop.src}
-          title="Nike Clone"
-          link="https://shop-nike1.netlify.app/"
-        />
-        <ProjectCard
-          img={dynamicPortfolio.src}
-          title="Tea House"
-          link="https://abu-0-rayhan.github.io/Tea-House/"
-        />
+       
 
-        <ProjectCard
-          img={dashboard.src}
-          title="Usability Hub"
-          link="https://helpful-bubblegum-1cb14f.netlify.app/"
-        />
+        
       </div>
     </div>
   );
